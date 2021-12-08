2a8fa80c21d474db63a50d2198967750c199aa22
GUILHERME SILVA NUNES <GUILHERME.SNUNES@ibm.com>
2021-12-07 21:07:38 -0300

    First Commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    static defaultProps = {
+        title: 'GitHub Finder',
+        icon: 'fab fa-github'
+    }
+    render() {
+        return (
+            <nav className='navbar bg-dark'>
+                <h1>
+                    <i className={this.props.icon}/> {this.props.title}
+                </h1>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
